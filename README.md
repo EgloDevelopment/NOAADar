3b7465887a8bfeaa8da1b4dbd070d2760c84656b
Eglo <UNUstifA4W346TcF2Qwc@gmail.com>
2024-07-07 20:25:19 -0500

    idk man

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # NOAADar
 System used to detect undocumented boats and large ships using neural networks and NOAA buoys across the coast.
+
+# Installation
+This installation is designed specifically for a fresh installation of  Alpine Linux
+
+```
+apk update
+apk add nano
+cp config.example.json config.json
+nano config.json
+```
+
+Now is when you should update your variables to make the application work
+
+```
+chmod +x install.sh
+./install.sh
+```
+
+The app should now be deployed using pm2 to run every 10 minutes
--- install.sh
+++ install.sh
@@ -3,35 +3,33 @@
 # Check if Python is installed
 if ! command -v python3 &> /dev/null; then
     echo "Python is not installed. Installing Python..."
-    sudo apk update
-    sudo apk add python3
+    apk update
+    apk add python3
 else
     echo "Python is already installed."
-fi
-
-# Check if NPM is installed
-if ! command -v npm &> /dev/null; then
-    echo "NPM is not installed. Installing NPM..."
-    sudo apk update
-    sudo apk add npm
-else
-    echo "NPM is already installed."
 fi
 
 # Check if Node.js is installed
 if ! command -v node &> /dev/null; then
     echo "Node.js is not installed. Installing Node.js..."
-    sudo apk add curl
-    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E sh -
-    sudo apk add nodejs
+    apk add nodejs
 else
     echo "Node.js is already installed."
+fi
+
+# Check if NPM is installed
+if ! command -v npm &> /dev/null; then
+    echo "NPM is not installed. Installing NPM..."
+    apk update
+    apk add npm
+else
+    echo "NPM is already installed."
 fi
 
 # Install pm2 globally
 if ! command -v pm2 &> /dev/null; then
     echo "pm2 is not installed. Installing pm2 globally..."
-    sudo npm install -g pm2
+    npm install -g pm2
 else
     echo "pm2 is already installed."
 fi
